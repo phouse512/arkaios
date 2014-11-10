b96573730ebacba380a7ab4d4360dfc65c6a0951
phouse512 <philiphouse2015@u.northwestern.edu>
2014-11-09 18:20:07 -0600

    heroku push

--- arkaios/config.py
+++ arkaios/config.py
@@ -3,8 +3,8 @@ PWD = os.path.abspath(os.curdir)
 
 DEBUG=True
 #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_DATABASE_URI = 'postgres://PhilipHouse:house@localhost/arkaios'
-#SQLALCHEMY_DATABASE_URI = 'postgres://gtwnxaeulqztlh:Af5wrfurq510fqonyoFiZryaFg@ec2-54-197-241-91.compute-1.amazonaws.com:5432/d5uaju8veb38j0'
+#SQLALCHEMY_DATABASE_URI = 'postgres://PhilipHouse:house@localhost/arkaios'
+SQLALCHEMY_DATABASE_URI = 'postgres://gtwnxaeulqztlh:Af5wrfurq510fqonyoFiZryaFg@ec2-54-197-241-91.compute-1.amazonaws.com:5432/d5uaju8veb38j0'
 SECRET_KEY = 'thisissecret'
 SESSION_PROTECTION = 'stroang'
 CSRF_ENABLED = True
